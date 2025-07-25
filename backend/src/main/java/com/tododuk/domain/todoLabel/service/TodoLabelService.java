fc3b35fa2e897b01825fa0d72e022849adfa81c6
LeeKW <lkw9241@gmail.com>
2025-07-25 21:29:49 +0900

    feature : add label

--- backend/src/main/java/com/tododuk/domain/todoLabel/service/TodoLabelService.java
+++ backend/src/main/java/com/tododuk/domain/todoLabel/service/TodoLabelService.java
@@ -6,6 +6,8 @@ import com.tododuk.domain.todoLabel.repository.TodoLabelRepository;
 public class TodoLabelService {
     TodoLabelRepository todoLabelRepository;
 
+
+
     public void removeTodoLabelFromTodo(int todoId, int labelId){
         TodoLabel todoLabel = todoLabelRepository.findByTodoIdAndLabelId(todoId, labelId)
                 .orElseThrow(()->new IllegalArgumentException("todoLabel not found"));
