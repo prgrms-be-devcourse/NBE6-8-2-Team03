d57ffd1026a9efe56762ac96fcf33acc9194f533
KSH0326 <ths0074@naver.com>
2025-07-28 14:13:52 +0900

    [BE]feat: 회원가입,로그인 기초 구현+가입test구현
    # Conflicts:
    #	backend/src/main/java/com/tododuk/domain/user/entity/User.java

--- backend/src/main/java/com/tododuk/domain/user/controller/UserController.java
+++ backend/src/main/java/com/tododuk/domain/user/controller/UserController.java
@@ -1,11 +1,95 @@
 package com.tododuk.domain.user.controller;
 
+import com.tododuk.domain.user.dto.UserDto;
+import com.tododuk.domain.user.entity.User;
 import com.tododuk.domain.user.service.UserService;
+import com.tododuk.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
 public class UserController {
     private final UserService userService;
+
+    record UserJoinReqDto(
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String email,
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String password,
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String nickname
+    ) {
+    }
+
+    @PostMapping("/register")
+    public RsData<UserDto> join(
+            @Valid @RequestBody UserJoinReqDto reqBody
+    ){
+        userService.findByUserEmail(reqBody.email)
+                .ifPresent(_user -> {
+                    throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+                });
+
+        User user = userService.join(
+                reqBody.email(),
+                reqBody.password(),
+                reqBody.nickname()
+        );
+
+        return new RsData<>(
+                "201-1",
+                "%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(user.getNickName()),
+                new UserDto(user)
+        );
+    }
+
+    record UserLoginReqDto(
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String email,
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String password
+    ) {
+    }
+
+    record UserLoginResDto(
+
+            UserDto userDto,
+            String apiKey
+    ) {
+    }
+    @PostMapping("/login")
+    public RsData<UserLoginResDto> login(
+            @Valid @RequestBody UserLoginReqDto reqBody
+    ) {
+        User user = userService.findByUserEmail(reqBody.email)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."));
+
+        if (!user.getPassword().equals(reqBody.password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        //dto 안에 기본 정보만 포함되어있음
+        return new RsData<>(
+                "201-1",
+                "%s님 환영합니다.".formatted(user.getNickName()),
+                new UserLoginResDto(
+                        new UserDto(user),
+                        user.getApiKey()
+                )
+        );
+    }
+
 }
---
+++ backend/src/test/java/com/tododuk/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,63 @@
+package com.tododuk.domain.user.controller;
+
+import com.tododuk.domain.user.entity.User;
+import com.tododuk.domain.user.service.UserService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class UserControllerTest {
+    @Autowired
+    private UserService UserService;
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @DisplayName("회원가입")
+    void t1() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/user")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "email": "usernew@gmail.com",
+                                            "password": "1234",
+                                            "nickname": "무명"
+                                        }
+                                        """.stripIndent())
+                )
+                .andDo(print());
+
+        User user = userService.findByUserEmail("usernew@gmail.com").get();
+
+        resultActions
+                .andExpect(handler().handlerType(UserController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(user.getNickName())))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id").value(user.getId()))
+                .andExpect(jsonPath("$.data.createDate").value(Matchers.startsWith(user.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.modifyDate").value(Matchers.startsWith(user.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.email").value(user.getUserEmail()));
+    }
+}
