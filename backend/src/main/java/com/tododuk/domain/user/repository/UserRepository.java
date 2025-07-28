334f3fbcb11489b7365b10340cc747be1e0c3dd4
KSH0326 <ths0074@naver.com>
2025-07-28 09:42:13 +0900

    [BE] feature: service,controller 추가

--- backend/src/main/java/com/tododuk/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/tododuk/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.tododuk.domain.user.repository;
 
 import com.tododuk.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
---
+++ backend/src/main/java/com/tododuk/domain/user/service/UserService.java
@@ -0,0 +1,12 @@
+package com.tododuk.domain.user.service;
+
+import com.tododuk.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+}
