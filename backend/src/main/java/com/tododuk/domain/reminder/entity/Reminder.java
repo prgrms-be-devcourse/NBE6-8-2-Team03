c93ab6f7863412708d21d5eb162362fbb9977444
moon472907 <moon472907@gmail.com>
2025-07-29 14:35:59 +0900

    리마인더 엔티티 crud및 api추기

--- backend/src/main/java/com/tododuk/domain/reminder/entity/Reminder.java
+++ backend/src/main/java/com/tododuk/domain/reminder/entity/Reminder.java
@@ -6,16 +6,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Reminder extends BaseEntity {
     @ManyToOne
     private Todo todo;
-
     private LocalDateTime remindAt;
     private String method; //알림 내용? 알림 방법?
+
+    public Reminder(Todo todo, LocalDateTime remindAt, String method) {
+        this.todo = todo;
+        this.remindAt = remindAt;
+        this.method = method;
+    }
+
 }
