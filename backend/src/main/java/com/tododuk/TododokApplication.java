9b81cee5c281867f6a0128d8eb403e68f9d140ef
moon472907 <moon472907@gmail.com>
2025-07-29 14:38:22 +0900

    어플리케이션에 EnableJpaAuditing 어노테이션 추가

--- backend/src/main/java/com/tododuk/TododokApplication.java
+++ backend/src/main/java/com/tododuk/TododokApplication.java
@@ -2,8 +2,11 @@ package com.tododuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+
+@EnableJpaAuditing
 public class TododokApplication {
 
     public static void main(String[] args) {
