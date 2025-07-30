8ed09d6a69278ef515d588153080369f1806d693
KSH0326 <ths0074@naver.com>
2025-07-30 10:33:54 +0900

    [BE] 직전 커밋 누락 반영

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -35,6 +35,10 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
     implementation ("org.springframework.boot:spring-boot-starter-quartz")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.withType<Test> {
--- backend/src/main/java/com/tododuk/domain/user/service/AuthTokenService.java
+++ backend/src/main/java/com/tododuk/domain/user/service/AuthTokenService.java
@@ -1,4 +1,7 @@
 package com.tododuk.domain.user.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthTokenService {
 }
