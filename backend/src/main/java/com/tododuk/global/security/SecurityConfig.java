385ccb8750fa116dd2a15be8357ce7c5f404af3e
KSH0326 <ths0074@naver.com>
2025-07-30 17:39:49 +0900

    이전 커밋 재반영

--- backend/src/main/java/com/tododuk/global/security/SecurityConfig.java
+++ backend/src/main/java/com/tododuk/global/security/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.tododuk.global.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+
+                        auth -> auth
+                                //웹 아이콘 접근 허용
+                                .requestMatchers("/favicon.ico").permitAll()
+                                // H2 콘솔 접근 허용
+                                .requestMatchers("/h2-console/**").permitAll()
+                                // 접근 허용
+                                .requestMatchers("/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .headers(
+                        headers -> headers
+                                .frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                                )
+                        // csrf 설정 끔 (rest api에서는 csrf를 사용하지 않음)
+                ).csrf(
+                        AbstractHttpConfigurer::disable
+                );
+
+        return http.build();
+    }
 }
