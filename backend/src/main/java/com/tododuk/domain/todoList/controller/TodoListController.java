dcc2a00f7c6fd0100ae40475bc233b472315a55a
SoonTaeYouL <smile971021@naver.com>
2025-08-02 19:15:47 +0900

    [BE] feature:todolist 1차

--- backend/src/main/java/com/tododuk/domain/todoList/controller/TodoListController.java
+++ backend/src/main/java/com/tododuk/domain/todoList/controller/TodoListController.java
@@ -1,17 +1,17 @@
 package com.tododuk.domain.todoList.controller;
 
 
+import com.tododuk.domain.todoList.dto.TodoListReqDto;
 import com.tododuk.domain.todoList.dto.TodoListResponseDto;
+import com.tododuk.domain.todoList.entity.TodoList;
 import com.tododuk.domain.todoList.service.TodoListService;
+import com.tododuk.global.exception.ServiceException;
 import com.tododuk.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +28,53 @@ public class TodoListController {
     @Operation(summary = "전체 todolist 조회")
     public ResponseEntity<RsData<List<TodoListResponseDto>>> getAllTodoLists() {
         List<TodoListResponseDto> todolist = todoListService.getAllTodoLists();
-        return ResponseEntity.ok(RsData.success("전체 todo 조회 성공", todolist));
+        return ResponseEntity.ok(RsData.success("전체 todolist 조회 성공", todolist));
+    }
+
+    @GetMapping("/{list_id}")
+    @Transactional
+    @Operation(summary = "개별 todoList 조회")
+    public ResponseEntity<RsData<TodoListResponseDto>> getTodoListById(@PathVariable Integer todoList_id){
+        TodoListResponseDto list = todoListService.getTodoList(todoList_id);
+        return ResponseEntity.ok(RsData.success("todolist 조회 성공", list));
+    }
+
+    @PostMapping
+    @Transactional
+    @Operation(summary = "todolist 생성")
+    public ResponseEntity<RsData<TodoList>> addTodoList(
+            @RequestBody TodoListReqDto reqDto
+    ) {
+        TodoList saveList = todoListService.addTodoList(reqDto);
+        return ResponseEntity.ok(RsData.success("새로운 todo 생성 성공", saveList));
+    }
+
+    @PutMapping(value = "/{list_id}")
+    @Transactional
+    @Operation(summary = "todolist 수정")
+    public ResponseEntity<RsData<TodoList>> updateTodoList(
+            @PathVariable Integer list_id,
+            @RequestBody TodoListReqDto reqDto
+    ) {
+        try {
+            TodoList todoList = todoListService.updateTodoList(list_id, reqDto);
+            return ResponseEntity.ok(RsData.success("todolist 수정 성공", todoList));
+        } catch (Exception e) {
+            throw new ServiceException("400-1", "수정에 실패하였습니다.");
+        }
+    }
+
+    @DeleteMapping("/{list_id}")
+    @Transactional
+    @Operation(summary = "todolist 삭제")
+    public ResponseEntity<RsData<TodoList>> deleteTodoList(
+            @PathVariable Integer list_id
+    ) {
+        try {
+            todoListService.deleteTodoList(list_id);
+            return ResponseEntity.ok(RsData.success("todolist 삭제성공"));
+        } catch (Exception e) {
+            throw new ServiceException("400-1", "삭제에 실패하였습니다.");
+        }
     }
 }
--- backend/src/main/java/com/tododuk/domain/todoList/service/TodoListService.java
+++ backend/src/main/java/com/tododuk/domain/todoList/service/TodoListService.java
@@ -1,7 +1,13 @@
 package com.tododuk.domain.todoList.service;
 
+import com.tododuk.domain.team.entity.Team;
+import com.tododuk.domain.team.repository.TeamRepository;
+import com.tododuk.domain.todoList.dto.TodoListReqDto;
 import com.tododuk.domain.todoList.dto.TodoListResponseDto;
+import com.tododuk.domain.todoList.entity.TodoList;
 import com.tododuk.domain.todoList.repository.TodoListRepository;
+import com.tododuk.domain.user.entity.User;
+import com.tododuk.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +18,56 @@ import java.util.List;
 public class TodoListService {
 
     private final TodoListRepository todoListRepository;
+    private final UserRepository userRepository;
+    private final TeamRepository teamRepository;
 
     public List<TodoListResponseDto> getAllTodoLists() {
         return todoListRepository.findAll().stream()
                 .map(TodoListResponseDto::from)
                 .toList();
+    }
+
+    public TodoListResponseDto getTodoList(int id){
+        TodoList todoList = todoListRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 todolistid는 존재하지 않습니다."));
+        return TodoListResponseDto.from(todoList);
+    }
+
+    public TodoList addTodoList(TodoListReqDto reqDto) {
+        User user = userRepository.findById(reqDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
+
+        Team team = teamRepository.findById(reqDto.getTeamId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 팀이 없습니다."));
+        TodoList todoList = new TodoList(
+                reqDto.getName(),
+                reqDto.getDescription(),
+                user, team
+        );
+        return todoListRepository.save(todoList);
+    }
+
+    public TodoList updateTodoList(Integer listId, TodoListReqDto reqDto) {
+        TodoList todoList = todoListRepository.findById(listId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 todolist가 존재하지 않습니다."));
+
+        User user = userRepository.findById(reqDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
+
+        Team team = teamRepository.findById(reqDto.getTeamId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 팀이 없습니다."));
+
+        todoList.setName(reqDto.getName());
+        todoList.setDescription(reqDto.getDescription());
+        todoList.setUser(user);
+        todoList.setTeam(team);
+
+        return todoListRepository.save(todoList);
+    }
+
+    public void deleteTodoList(Integer listId) {
+        TodoList todoList = todoListRepository.findById(listId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 todolist는 존재하지 않습니다."));
+        todoListRepository.delete(todoList);
     }
 }
