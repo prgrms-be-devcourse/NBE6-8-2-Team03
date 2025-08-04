c3213cc50ca0613567f91ffe586551093e90a380
SoonTaeYouL <smile971021@naver.com>
2025-08-04 15:30:01 +0900

    [BE] feature:사용자의 todo/list조회

--- backend/src/main/java/com/tododuk/domain/todoList/controller/TodoListController.java
+++ backend/src/main/java/com/tododuk/domain/todoList/controller/TodoListController.java
@@ -5,12 +5,15 @@ import com.tododuk.domain.todoList.dto.TodoListReqDto;
 import com.tododuk.domain.todoList.dto.TodoListResponseDto;
 import com.tododuk.domain.todoList.entity.TodoList;
 import com.tododuk.domain.todoList.service.TodoListService;
+import com.tododuk.domain.user.entity.User;
+import com.tododuk.domain.user.service.UserService;
 import com.tododuk.global.exception.ServiceException;
 import com.tododuk.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +27,7 @@ import java.util.List;
 public class TodoListController {
 
     private final TodoListService todoListService;
+    private final UserService userService;
 
     @GetMapping
     @Transactional
@@ -88,5 +92,27 @@ public class TodoListController {
     ) {
         List<TodoListResponseDto> todoLists = todoListService.getUserTodoList(user_id);
         return ResponseEntity.ok(RsData.success("유저의 todo list 조회 성공", todoLists));
+    }
+
+    @GetMapping("/me")
+    @Transactional
+    @Operation(summary = "사용자의 투두리스트 조회")
+    public ResponseEntity<RsData<List<TodoListResponseDto>>> getMyTodoList(
+            Authentication authentication
+    ) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).body(new RsData<>("401-1", "인증이 필요합니다."));
+        }
+        String username = authentication.getName();
+        System.out.println("Authenticated Username: " + username);
+        User user = userService.findByUserEmail(username)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        try {
+            List<TodoListResponseDto> todoLists = todoListService.getUserTodoList(user.getId());
+            return ResponseEntity.ok(RsData.success("유저의 todo list 조회 성공", todoLists));
+        } catch (Exception e) {
+            throw new ServiceException("400-1", "list가 존재하지 않습니다.");
+        }
     }
 }
