ac0cdea027a211a222cdaad396c97dfbcfb88669
darancode <darancode@gmail.com>
2025-08-05 12:37:16 +0900

    fix(fe): 깃헙 리베이스 충돌 문제 해결

--- backend/src/main/java/com/tododuk/domain/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/tododuk/domain/todo/repository/TodoRepository.java
@@ -13,3 +13,19 @@ public interface TodoRepository extends JpaRepository <Todo, Integer> {
     // 할일 목록 ID로 할일 조회
     List<Todo> findByTodoListId(Integer todoListId);
 }
+<<<<<<< HEAD
+=======
+=======
+
+<<<<<<< HEAD
+=======
+    
+    // TodoList ID로 할일 목록 조회 (생성일 순으로 정렬)
+    List<Todo> findByTodoListIdOrderByCreateDateDesc(Integer todoListId);
+>>>>>>> ca82fbd (backup(fe): 팀 투두 서비스 철폐)
+}
+>>>>>>> 9b69a65 (backup(fe): 팀 투두 서비스 철폐)
+=======
+}
+>>>>>>> cad2c1e (fix(fe): 깃헙 리베이스 충돌 문제 해결)
+>>>>>>> 54c89b4 (fix(fe): 깃헙 리베이스 충돌 문제 해결)
