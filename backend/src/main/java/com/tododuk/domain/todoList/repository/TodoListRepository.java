5075eee4a0c77017618fac857d85641dc992f6b2
darancode <darancode@gmail.com>
2025-08-05 11:21:00 +0900

    팀 투두리스트, 팀 투두 엔티티 및 관련 서비스 만들어서 백업

--- backend/src/main/java/com/tododuk/domain/todoList/repository/TodoListRepository.java
+++ backend/src/main/java/com/tododuk/domain/todoList/repository/TodoListRepository.java
@@ -4,10 +4,7 @@ import com.tododuk.domain.todoList.entity.TodoList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TodoListRepository extends JpaRepository<TodoList, Integer> {
     List<TodoList> findAllByUserId(Integer userId);
-    Optional<TodoList> findByUserIdAndTeamIdIsNull(Integer userId);
-    Optional<TodoList> findByTeamId(Integer teamId);
 }
